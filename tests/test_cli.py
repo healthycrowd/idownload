662cae828df6d5f921951b21dc3ed9ccf063fe7f
Matt Wisniewski <hello@mattw.life>
2023-07-07 14:29:04 -0400

    test stubs

---
+++ tests/test_cli.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.mark.skip()
+def test_cli_success():
+    pass
+
+
+@pytest.mark.skip()
+def test_command_success():
+    pass
+
+
+@pytest.mark.skip()
+def test_command_fail():
+    pass
---
+++ tests/test_pinterest.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+@pytest.mark.skip()
+def test_download_success_normal():
+    pass
+
+
+@pytest.mark.skip()
+def test_download_success_alternate_suffix():
+    pass
+
+
+@pytest.mark.skip()
+def test_download_success_ignore_filename_duplicates():
+    pass
+
+
+@pytest.mark.skip()
+def test_download_success_ignore_fnum_duplicates():
+    pass
+
+
+@pytest.mark.skip()
+def test_download_fail_imagedownload():
+    pass
